8a971779ba665a0b845be78fb659c9dcfe0ca61f
JwonMoon <mjiw9440@naver.com>
2025-05-26 02:28:37 +0900

    ADD backbone branch code

---
+++ B2D_tcp/Bench2Drive/leaderboard/scripts/ros_run_tcp_branch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# export PYTHONPATH=$PYTHONPATH:~/shared_dir/B2D_Demo/B2D_tcp/Bench2Drive/Bench2DriveZoo #jw) TCP import error
+source /opt/ros/humble/setup.bash
+export PYTHONPATH=$PYTHONPATH:$(eval echo ~/shared_dir/B2D_Demo/B2D_tcp/Bench2Drive/Bench2DriveZoo)
+
+export CKPT_PATH='./../Bench2DriveZoo/ckpts/tcp_b2d.ckpt'
+export SAVE_PATH='./eval_v1/'
+export DEBUG=True
+export LEADERBOARD_ROOT=leaderboard
+export PLANNER_TYPE='merge_ctrl_traj'
+# export PLANNER_TYPE='only_traj'
+# export PLANNER_TYPE='only_ctrl'
+
+/usr/bin/python3 ${LEADERBOARD_ROOT}/team_code/tcp_branch_node.py \
+--ckpt-path=${CKPT_PATH} \
+--save-path=${SAVE_PATH} \
+--debug=${DEBUG}
