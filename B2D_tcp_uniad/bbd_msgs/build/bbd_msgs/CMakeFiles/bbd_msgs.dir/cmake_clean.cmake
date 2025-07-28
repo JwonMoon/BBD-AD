a129e0a5eb616998850763ccfc1e541a2f257c3a
Jiwon <65356458+JwonMoon@users.noreply.github.com>
2025-07-28 18:51:54 +0900

    Delete B2D_tcp_uniad/bbd_msgs/build directory

--- B2D_tcp_uniad/bbd_msgs/build/bbd_msgs/CMakeFiles/bbd_msgs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bbd_msgs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bbd_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- B2D_tcp_uniad/bbd_msgs/build/bbd_msgs/bbd_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/shared_dir/B2D_Demo/B2D_tcp_uniad/bbd_msgs/build/bbd_msgs/bbd_msgs__py
-# Build directory: /root/shared_dir/B2D_Demo/B2D_tcp_uniad/bbd_msgs/build/bbd_msgs/bbd_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
