db80e9537915160766ff237669971e165c685ad0
Jiwon <65356458+JwonMoon@users.noreply.github.com>
2025-07-28 18:52:02 +0900

    Delete B2D_tcp_uniad/bbd_msgs/install directory

--- B2D_tcp_uniad/bbd_msgs/install/bbd_msgs/lib/python3.8/site-packages/bbd_msgs/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from bbd_msgs.msg._bbd_backbone_output import BBDBackboneOutput  # noqa: F401
-from bbd_msgs.msg._bbd_branch_output import BBDBranchOutput  # noqa: F401
-from bbd_msgs.msg._tick_trigger import TickTrigger  # noqa: F401
--- B2D_tcp_uniad/bbd_msgs/install/bbd_msgs/share/bbd_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(bbd_msgs_IDL_FILES "msg/BBDBackboneOutput.idl;msg/BBDBranchOutput.idl;msg/TickTrigger.idl")
-set(bbd_msgs_INTERFACE_FILES "msg/BBDBackboneOutput.msg;msg/BBDBranchOutput.msg;msg/TickTrigger.msg")
