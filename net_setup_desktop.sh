4a27bd28e57b689fe77299c098bb7de3f94ef463
JwonMoon <mjiw9440@naver.com>
2025-07-31 17:02:11 +0900

    MOD net_setup_desktop.sh

--- net_setup_desktop.sh
+++ net_setup_desktop.sh
@@ -2,6 +2,6 @@
 
 sudo ip addr add 192.168.10.1/24 dev eno1
 sudo ip addr add 192.168.30.2/24 dev enx00e04c592990
-echo "[Orin2 Setup] Network interfaces configured successfully."
+echo "[Desktop Setup] Network interfaces configured successfully."
 
 
