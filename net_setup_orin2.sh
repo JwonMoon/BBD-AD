ddba0fdbe744a31450619aa8262d5897ba4c64ce
JwonMoon <mjiw9440@naver.com>
2025-07-31 17:05:18 +0900

    MOD net_setup_orin2.sh

--- net_setup_orin2.sh
+++ net_setup_orin2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # == Modify to suit your environment == 
-sudo ip addr add 192.168.20.2/24 dev eno1
-sudo ip addr add 192.168.30.1/24 dev enx00e04c5a7590
+sudo ip addr add 192.168.20.2/24 dev eth1
+sudo ip addr add 192.168.30.1/24 dev eth2
 echo "[Orin2 Setup] Network interfaces configured successfully."
